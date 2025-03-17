2edc8446c1b5e20eee01722bde9e9799b538ff92
Oussama Khouya <oussama.khouya-etu@etu.univh2c.ma>
2025-03-17 15:06:16 +0000

    ajouter pagination au tableau des patients

--- src/main/java/khouya/site/Hopital/web/PatientController.java
+++ src/main/java/khouya/site/Hopital/web/PatientController.java
@@ -1,11 +1,15 @@
 package khouya.site.Hopital.web;
 
+import khouya.site.Hopital.entities.Patient;
 import khouya.site.Hopital.repository.PatientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
@@ -14,8 +18,13 @@ public class PatientController {
     private PatientRepository patientRepo;
 
     @GetMapping("/index")
-    public String index(Model model){
-        model.addAttribute("listPatients", patientRepo.findAll());
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int page,
+                        @RequestParam(name = "size", defaultValue = "4")int size){
+        Page<Patient> pagePatients = patientRepo.findAll(PageRequest.of(page, size));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
         return "patients";
     }
 }
