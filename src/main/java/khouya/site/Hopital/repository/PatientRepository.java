3e257e5bda3a22d31bd6189a0cd52532d560311e
Oussama Khouya <oussama.khouya-etu@etu.univh2c.ma>
2025-03-17 15:27:50 +0000

    ajouter formulaire re recherche

--- src/main/java/khouya/site/Hopital/repository/PatientRepository.java
+++ src/main/java/khouya/site/Hopital/repository/PatientRepository.java
@@ -1,7 +1,17 @@
 package khouya.site.Hopital.repository;
 
 import khouya.site.Hopital.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    Page<Patient> findPatientsByNomContaining(String keyword, Pageable pageable);
+
+
+    @Query("select p from Patient p where p.nom like :x")
+    Page<Patient> chercher(@Param("x") String keyword, Pageable pageable);
 }
--- src/main/java/khouya/site/Hopital/web/PatientController.java
+++ src/main/java/khouya/site/Hopital/web/PatientController.java
@@ -20,11 +20,13 @@ public class PatientController {
     @GetMapping("/index")
     public String index(Model model,
                         @RequestParam(name = "page", defaultValue = "0") int page,
-                        @RequestParam(name = "size", defaultValue = "4")int size){
-        Page<Patient> pagePatients = patientRepo.findAll(PageRequest.of(page, size));
+                        @RequestParam(name = "size", defaultValue = "4")int size,
+                        @RequestParam(name = "keyword", defaultValue = "")String keyword){
+        Page<Patient> pagePatients = patientRepo.findPatientsByNomContaining(keyword, PageRequest.of(page, size));
         model.addAttribute("listPatients", pagePatients.getContent());
         model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
         model.addAttribute("currentPage", page);
+        model.addAttribute("keyword", keyword);
         return "patients";
     }
 }
